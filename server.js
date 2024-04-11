82670fa9ec63a884129bfcb341a35d9ed415b806
KoushiikMuralitharan <137808333+KoushiikMuralitharan@users.noreply.github.com>
2024-04-11 23:32:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,17 +18,15 @@
                 const user = arr.find((user) => user.email == username.value && user.password == password.value);
                     if(user){
                         //alert('Login successfull!');
-                        window.location.href="http://127.0.0.1:3000/index.html"
+                        window.location.href='./Content.html';
                     }else{
                         //alert('Login failed');
-                        window.location.href="http://127.0.0.1:3000/signup.html";
+                        window.location.href='./signup.html';
                     }
             })
             .catch((error) => {
                 console.error("Fetch error:", error);
             });
-           })
-            
-            
+           })   
         });
     });
